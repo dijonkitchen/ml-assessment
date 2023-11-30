06001b6a194aa790981e4dec04b6261c88f0c4bb
JC (Jonathan Chen) <11434205+dijonkitchen@users.noreply.github.com>
2023-11-29 19:51:27 -0500

    refactor(perf): optimize algorithm further

--- README.md
+++ README.md
@@ -7,3 +7,43 @@ A successful ML app engineer will be able to contribute to our ML ambitions. Thi
 ## Instructions
 
 There are three assessments included. The API and model assessments both directly relate to the kind of tools and work relevant to the position. The algorithm assessment is intended to test problem solving and understanding of the python language. You only need to pick two of these assessments to do. The intention is that this shouldn't take more than an hour or two to complete. Be prepared to discuss and extend your solutions.
+
+## Setup and install
+
+* Homebrew: https://brew.sh/
+* pyenv: https://github.com/pyenv/pyenv#installation
+* Poetry: https://python-poetry.org/docs/#installing-with-pipx
+
+## Development
+
+### Activate virtual environment:
+
+```bash
+poetry shell
+```
+
+### Exit virtual environment:
+
+```bash
+exit
+```
+
+### Install dependencies:
+
+```bash
+poetry install
+```
+
+To add new dependencies:
+```bash
+poetry add <dependency>
+```
+
+### Running
+
+#### Algorithms
+
+```
+poetry run python algorithms/main.py
+```
+All tests passed in 0.9625127500039525 seconds!
--- algorithms/main.py
+++ algorithms/main.py
@@ -1,60 +1,9 @@
-from itertools import permutations
 import time
+from itertools import permutations
 
-
-def play_countdown(letters: str, words = None) -> str:
-    """
-    Plays the game of countdown with the given letters.
-    """
-    # Brute force: Time complexity: O(letters! * words), Space complexity: O(letters!)
-    # Generate all permutations of possible words given letters. Time complexity: O(letters!), Space complexity: O(letters!)
-
-    # Search wordlist for each possible word. Time: O(letters! * words)
-
-    # If actual word
-        # Save to new list. Space: O(letters!)
-
-    # Loop through each actual word for longest word. Space: O(letters!)
-
-    # Return longest word
-
-
-    # Optimized: Time complexity: O(letters!), Space complexity: O(letters! + words)
-    # Set longest word to empty string. Space: O(1)
-
-    # Generate all permutations of possible words given letters. Time complexity: O(letters!), Space complexity: O(letters!)
-
-    # Generate a dictionary based on wordlist if it doesn't exist. Time: O(words), Space: O(words)
-
-    # Search for each possible word. Time: O(letters!)
-
-    # If actual word and length is greater than longest word,
-        # If same length as letters, return early
-        # Else, save as longest word
-
-    # Return longest word
-
-    longest = ""
-
-    possible_words = []
-
-    for i in range(1, len(letters) + 1):
-        possible_words += [''.join(p) for p in permutations(letters, i)]
-
-    if not words:
-        words = create_dictionary('algorithms/words_alpha.txt')
-
-    for possible_word in possible_words:
-        if words.get(possible_word) and (len(possible_word) > len(longest)):
-            if len(possible_word) == len(letters):
-                return possible_word
-
-            longest = possible_word
-
-    return longest
 
 def create_dictionary(file_path: str) -> dict[str, True]:
-    with open(file_path, 'r') as file:
+    with open(file_path, 'r', encoding="utf-8") as file:
         data = file.readlines()
 
     words = {}
@@ -65,10 +14,67 @@ def create_dictionary(file_path: str) -> dict[str, True]:
     return words
 
 
+def play_countdown(letters: str, words=None) -> str:
+    """
+    Plays the game of countdown with the given letters.
+    """
+    # Brute force: Time complexity: O(letters! * words), Space complexity: O(letters!)
+    # Generate all permutations of possible words given letters. Time complexity: O(letters!), Space complexity: O(letters!)
+
+    # Search wordlist for each possible word. Time: O(letters! * words)
+
+    # If actual word
+    #   Save to new list. Space: O(letters!)
+
+    # Loop through each actual word for longest word. Space: O(letters!)
+
+    # Return longest word
+
+
+    # Better1: Time complexity: O(letters!), Space complexity: O(letters! + words)
+    # Set longest word to empty string. Space: O(1)
+
+    # Generate all permutations of possible words given letters. Time complexity: O(letters!), Space complexity: O(letters!)
+
+    # Generate a dictionary based on wordlist if it doesn't exist. Time: O(words), Space: O(words)
+
+    # Search for each possible word. Time: O(letters!)
+
+    # If actual word and length is greater than longest word,
+    #   If same length as letters, return early
+    #   Else, save as longest word
+
+    # Return longest word
+
+
+    # Better2: Time complexity: O(letters!), Space complexity: O(words) if not cached, O(1) if cached
+    # Generate a dictionary based on wordlist if it doesn't exist. Time: O(words), Space: O(words)
+
+    # Generate permutations of possible words given letters from most to least. Time complexity: O(letters!), Space complexity: O(1)
+    #   Search for each possible word. Time: O(letters!)
+
+    #   If actual word and length is greater than longest word,
+    #       If word found, return word
+
+    # Return empty string since none found
+    if not words:
+        words = create_dictionary('algorithms/words_alpha.txt')
+
+    for i in range(len(letters), 0, -1):
+        for perm in permutations(letters, i):
+            possible_word = ''.join(perm)
+
+            if words.get(possible_word):
+                return possible_word
+
+    return ""
+
+
 if __name__ == "__main__":
     tic = time.perf_counter()
 
     words = create_dictionary('algorithms/words_alpha.txt')
+
     assert len(play_countdown("sretpmocc", words)) == 8
     assert len(play_countdown("ndaelryra", words)) == 7
     assert len(play_countdown("terhbswoa", words)) == 9
